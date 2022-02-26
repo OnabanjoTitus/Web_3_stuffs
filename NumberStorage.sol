c45fec15daa17adfbd2b10d4ab5c171675d8dc9e
Onabanjo Titus <titus.onabanjo@arikair.com>
2022-02-26 23:17:27 +0100

    update

--- NumberStorage.sol
+++ NumberStorage.sol
@@ -1,16 +1,45 @@
 pragma solidity ^0.6.0;
 
-contract NumberStorage{
+contract SimpleStorage{
     
-   
-    uint256 public favouriteNumber;
+    // // this will be initialized to 0
+    // // this will be initialized to 0
+    uint256 favouriteNumber;
 
     function store(uint256 _favouriteNumber)public{
        favouriteNumber=_favouriteNumber;
-
-
     }
-    
+    //retrieve
+    function retrieve()public view returns(uint256) {
+        return favouriteNumber;
+    }
+
+    //perform a non saving operation
+    function retrieve(uint256 _favouriteNumber)public view returns(uint256) {   
+        return favouriteNumber+_favouriteNumber;
+    }
+
+    struct People{
+        uint256 favouriteNumber;
+        string name;
+    }
+
+People public person=People({favouriteNumber:2,name:"Titus"});
+
+People[]public people;
+
+function addPerson(string memory _name,uint256 _favouriteNumber)public{
+
+
+people.push(People({favouriteNumber:_favouriteNumber,name:_name}));
+
+//or
+// people.push(People(_favouriteNumber,_name));
+
+}
+
+
+
 
 
 }
