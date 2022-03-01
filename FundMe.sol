9db2c527737b59c31e527e6179c99981d87af20e
Onabanjo Titus <titus.onabanjo@arikair.com>
2022-03-01 17:10:20 +0100

    Fund Me

--- FundMe.sol
+++ FundMe.sol
@@ -9,6 +9,8 @@ contract FundMe{
 
     mapping(address => uint256) public addressToAmountFunded;
     function fund() public payable{
+        //$50
+
         addressToAmountFunded[msg.sender]+=msg.value;
         //what the ETH -> USD conversion rate
      }
@@ -24,8 +26,19 @@ contract FundMe{
          AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e); 
             (,int256 answer,,,
             )=priceFeed.latestRoundData();
-            return uint256(answer);
+            return uint256(answer * 10000000000);
+            
 
      }
 
+
+    function getConversionRate(uint256 amount) public view returns(uint256){
+        uint256 ethPrice= getPrice();
+        uint256 ethAmountInUSD=(ethPrice * amount)/1000000000000000000;
+        // 1000000000000000000
+        return ethAmountInUSD;
+        //0.000002967294590160
+
+
+    }
 }
