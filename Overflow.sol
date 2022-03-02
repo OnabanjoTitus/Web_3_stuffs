3e4446f56186718544e9bedd33f51bdd575fcc6d
Onabanjo Titus <titus.onabanjo@arikair.com>
2022-03-02 09:50:18 +0100

    OverFlow

--- Overflow.sol
+++ Overflow.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.6 <0.9.0;
+pragma solidity ^0.6.6;
+import "@chainlink/contracts/src/v0.6/interfaces/SafeMathChainlink.sol";
 
 contract Overflow{
-
+    using SafeMathChainlink for uint256;
+    
     function overflow() public view returns(uint8){
         //Integers reset once we reach the maximum cap
         uint8 big =255 + uint8(100);
